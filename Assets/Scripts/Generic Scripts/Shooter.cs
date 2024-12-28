eaf78291e47f4a3e8c87efaedd07792f83ba1500
Hooninator <jbellavita@berkeley.edu>
2024-12-27 16:36:08 -0800

    first pass shot

--- Assets/Scripts/Generic Scripts/Shooter.cs
+++ Assets/Scripts/Generic Scripts/Shooter.cs
@@ -2,18 +2,53 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+
+    public PlayerMovement player;
+
+    /**********************
+     * SHOOTING VARIABLES *
+     * ********************/
     public GameObject firingPoint;
     public float fireRate;
+    public Bullet dummyBullet;
+    public float timeSinceLastShot;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player.Shot += Shot;
+        timeSinceLastShot = float.PositiveInfinity;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastShot += Time.deltaTime;
     }
+
+
+    void Shot()
+    {
+        // Has it been enough time since the last shot
+        if (timeSinceLastShot >= fireRate)
+        {
+            // Instantiate the bullet from prefab at firing point
+            Bullet bullet = Instantiate(dummyBullet, firingPoint.transform.position, Quaternion.identity);
+
+            // Set parameters of the new bullet, epic gamer style
+            InitBullet(bullet);
+
+            timeSinceLastShot = 0.0f;
+        }
+    }
+
+
+    void InitBullet(Bullet bullet)
+    {
+        // This is stupid
+        bullet.direction = player.gameObject.transform.forward;
+    }
+
 }
