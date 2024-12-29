e18e90259d093839afc32cab6758018e11a37a02
Hooninator <jbellavita@berkeley.edu>
2024-12-29 13:51:08 -0800

    melt delay

--- Assets/Scripts/Generic Scripts/MeltableTiles.cs
+++ Assets/Scripts/Generic Scripts/MeltableTiles.cs
@@ -8,6 +8,7 @@ public class MeltableTiles : MonoBehaviour
     public Tilemap tilemap;
 
     static public float regenTime = 1.0f; //seconds
+    static public float meltTime = 1.0f; //seconds
 
     private class MeltedTile
     {
@@ -27,7 +28,31 @@ public class MeltableTiles : MonoBehaviour
         }
     };
 
+
+    //This is basically the same as melted tile
+    private class MeltingTile
+    {
+        public float timeMelting = 0.0f;
+        public Vector3Int pos {get; set;}
+        public TileBase tile {get; set;}
+
+        public MeltingTile() {this.pos=Vector3Int.zero; this.tile=null;}
+
+        public MeltingTile(Vector3Int pos, TileBase tile)
+        {
+            this.pos = pos;
+            this.tile = tile;
+        }
+
+        public bool ShouldMelt()
+        {
+            return timeMelting >= meltTime;
+        }
+
+    };
+
     List<MeltedTile> meltedTiles = new();
+    List<MeltingTile> meltingTiles = new();
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -41,6 +66,54 @@ public class MeltableTiles : MonoBehaviour
     void Update()
     {
         UpdateMeltedTiles();
+    }
+
+
+    void UpdateMeltingTile(Vector3Int pos)
+    {
+        //Using dictionary would be better for this, but this list should always be small so this is fine
+        MeltingTile meltingTile = new();
+        bool found = false;
+        foreach (var tile in meltingTiles)
+        {
+            if (tile.pos == pos)
+            {
+                meltingTile = tile;
+                found = true;
+                break;
+            }
+        }
+        if (!found) return;
+
+        meltingTile.timeMelting += Time.deltaTime;
+
+        if (meltingTile.ShouldMelt())
+        {
+            DoMelt(meltingTile.pos);
+            meltingTiles.Remove(meltingTile);
+        }
+    }
+
+
+    void RemoveMeltingTile(Collision2D col)
+    {
+        var pos = GetPos(col);
+
+        //Using a dictionary would be better for this, but this list should always be small so this is fine
+        MeltingTile meltingTile = new();
+        bool found = false;
+        foreach (var tile in meltingTiles)
+        {
+            if (tile.pos == pos)
+            {
+                meltingTile = tile;
+                found = true;
+                break;
+            }
+        }
+        if (!found) return;
+
+        meltingTiles.Remove(meltingTile);
     }
 
 
@@ -67,6 +140,31 @@ public class MeltableTiles : MonoBehaviour
     }
 
 
+    public void OnCollisionStay2D(Collision2D col)
+    {
+        PlayerMovement player;
+        if (col.gameObject.TryGetComponent<PlayerMovement>(out player))
+        {
+            //UpdateMeltingTile(col);
+            if (player.mode == Mode.FIRE_MODE)
+            {
+                MeltBelow(col); 
+            }
+
+        }
+    }
+
+
+    public void OnCollisionExit2D(Collision2D col)
+    {
+        PlayerMovement player;
+        if (col.gameObject.TryGetComponent<PlayerMovement>(out player))
+        {
+            RemoveMeltingTile(col);
+        }
+    }
+
+
     public void OnCollisionEnter2D(Collision2D col)
     {
         Bullet b;
@@ -85,7 +183,7 @@ public class MeltableTiles : MonoBehaviour
         {
             if (player.mode == Mode.FIRE_MODE)
             {
-                MeltBelow(col); //TODO: Delayed melt
+                MeltBelow(col); 
             }
         }
     }
@@ -93,7 +191,11 @@ public class MeltableTiles : MonoBehaviour
 
     public Vector3Int GetPos(Collision2D col)
     {
+        if (col.contactCount<1)
+            return new Vector3Int(-100000000, 10000000, 10);
+
         bool wasFound = false;
+
         Vector3 collisionPos = col.GetContact(0).point;
         Vector3Int tilePos = tilemap.WorldToCell(collisionPos);
 
@@ -119,6 +221,19 @@ public class MeltableTiles : MonoBehaviour
     }
 
 
+    public int FindMeltingTile(Vector3Int pos)
+    {
+        for(int i=0; i<meltingTiles.Count; i++)
+        {
+            var tile = meltingTiles[i];
+            if (tile.pos==pos)
+                return i;
+        }
+        return -1;
+    }
+
+
+
     public void MeltShot(Collision2D col)
     {
         // Get tile, destroy it if it exists in the tilemap
@@ -129,6 +244,10 @@ public class MeltableTiles : MonoBehaviour
 
     public void MeltBelow(Collision2D col)
     {
+        // Do not melt spikes
+        if (col.gameObject.TryGetComponent<SpikeTiles>(out _))
+            return;
+
         // Get tile, destroy it if it exists in the tilemap
         Vector3 collisionPos = col.GetContact(0).point;
         Vector3Int tilePos = tilemap.WorldToCell(collisionPos) + Vector3Int.down;
@@ -137,14 +256,22 @@ public class MeltableTiles : MonoBehaviour
         {
             Vector3Int offset = new Vector3Int(i, 0, 0);
 
-            // Do not melt spikes
-            if (col.gameObject.TryGetComponent<SpikeTiles>(out _))
-                break;
-
-            DoMelt(tilePos + offset);
+            //DoMelt(tilePos + offset);
+            if (FindMeltingTile(tilePos + offset)!=-1)
+                UpdateMeltingTile(tilePos+offset);
+            else
+                AddMeltingTile(tilePos + offset);
         }
 
     }
+
+
+    public void AddMeltingTile(Vector3Int tilePos)
+    {
+        if (tilemap.HasTile(tilePos))
+            meltingTiles.Add(new MeltingTile( tilePos, tilemap.GetTile(tilePos)) );
+    }
+
 
 
     public void DoMelt(Vector3Int tilePos)
